d9b366c60ea6d5dfe7dcf58055691e696fe5b811
ebi <ebipresh02@gmail.com>
2023-03-15 17:16:48 +0100

    first commit

---
+++ build/script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle").addEventListener("click", function(){
+    document.getElementById("nav").classList.toggle("-left-full")
+    document.getElementById("nav").classList.toggle("left-0")
+})
